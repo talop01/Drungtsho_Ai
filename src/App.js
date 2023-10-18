ec8131eb50848e70447d05b6aec1a280bca49fd3
Kuenzang Namgyel <118171849+talop01@users.noreply.github.com>
2023-10-19 03:21:57 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import {
+  InferenceContext,
+  InferenceResult,
+  PhotoCollector
+} from "landingai-react";
+
+const apiInfo = {
+  endpoint: `https://predict.app.landing.ai/inference/v1/predict?endpoint_id=948a84ae-3196-4fdc-8479-0b2332bc6073`,
+  key: "land_sk_slRRlIqbzxFagS9PbpbX5eCUEdYPrz9F1yBh0FW57VXMDRU2xJ"
+};
+
+export default function App() {
+  const [image, setImage] = useState();
+
+  return (
+    <InferenceContext.Provider value={apiInfo}>
+      <div style={{ textAlign: "center", marginBottom: 12 }}>
+        <img
+          src="https://th.bing.com/th?id=OIP.a63LyzsEhLF4cIq2Yda66gHaHa&w=249&h=249&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2"
+          width={30}
+        />
+        <h1>Drungtsho AI</h1>
+      </div>
+      <PhotoCollector setImage={setImage} />
+      <InferenceResult image={image} />
+      <p style={{ textAlign: "center" }}>
+        Powered by <b>NoMindBhutan</b>
+      </p>
+    </InferenceContext.Provider>
+  );
+}
